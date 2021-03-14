af147417e3c1bf4a99c068bfe1007a2781a7615a
freeman-lab <the.freeman.lab@gmail.com>
2021-03-14 11:04:57 -0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../theme'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Box } from 'theme-ui'
+
+const Index = () => {
+  return <Box sx={{fontSize: [5], px: [4], py: [4]}}>homepage</Box>
+}
+
+export default Index
