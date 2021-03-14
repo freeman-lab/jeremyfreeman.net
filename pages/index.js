d9218779b801a2a2ac240701b1311727574e3be0
freeman-lab <the.freeman.lab@gmail.com>
2021-03-14 11:38:30 -0700

    add link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import { Box } from 'theme-ui'
+import { Box, Link } from 'theme-ui'
 
 const Index = () => {
-  return <Box sx={{fontSize: [5], px: [4], py: [4]}}>homepage</Box>
+  return <Box sx={{px: [4], py: [4]}}>
+    <Box sx={{fontSize: [5]}}>homepage</Box>
+    <Link sx={{fontSize: [3]}} href='/blog'>blog</Link>
+  </Box>
 }
 
 export default Index
